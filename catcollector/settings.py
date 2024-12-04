954b894b8ed0ac3a0d1721248b68921ffd2ff6d0
XOKEESH <hello@kishamartin.com>
2024-12-04 11:11:26 -0800

    new changes

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -70,17 +70,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'catcollector.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catcollector',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
